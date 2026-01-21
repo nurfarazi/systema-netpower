da0e4defd1f1125315fd9dc5f180e366fb1a1408
farazi <nur369188@gmail.com>
2026-01-21 22:28:57 +0600

    Add initial project setup and configuration

--- client/src/main.server.ts
+++ client/src/main.server.ts
@@ -1,3 +1,6 @@
+// Disable certificate verification for development to allow self-signed certs
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
 import { BootstrapContext, bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { config } from './app/app.config.server';
